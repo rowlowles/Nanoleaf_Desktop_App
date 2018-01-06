4a42f0cfc51905ab67ecd3ab85253733ebce6b05
Robbie Lowles <robbie.windsor+git@gmail.com>
2018-01-05 23:44:37 -0500

    Added user interface to control aurora

--- Nanoleaf_Interface.py
+++ Nanoleaf_Interface.py
@@ -1,5 +1,7 @@
 from nanoleaf import setup, Aurora
-import sys
+from tkinter import *
+from tkinter import messagebox
+import os
 
 ips = None
 
@@ -23,8 +25,51 @@ def getEffects():
             effects.append(effect)
     return [effects, rhythm_effects]
 
+def controlWindow(effect_lists):
+    class Application(Frame):
+
+        def multiComb(self, name):
+            aurora.effect = name
+
+        def createWidgets(self):
+            Label(text="Nanoleaf Control" ,font=("Helvetica", 15)).pack()
+            left = Frame(root)
+            right = Frame(root)
+            left.pack(side = LEFT)
+            right.pack(side = RIGHT)
+
+            Label(text = "Effects",font=("Helvetica", 12)).pack(in_ = left)
+            Label(text = "Rhythms",font=("Helvetica", 12)).pack(in_ = right)
+
+            for effect in effect_lists[0]:
+                # Create N buttons, one for each N items in list, and create functions for each
+                Button(text=effect, command=lambda x=effect: self.multiComb(x), bd=2, height=2, font=
+                ('Helvetica', 10), width=20).pack(in_ = left)
+
+            for rhy_effect in effect_lists[1]:
+                Button(text=rhy_effect, command=lambda x=rhy_effect: self.multiComb(x), bd=2, height=2, font=
+                ('Helvetica', 10), width=20).pack(in_ = right)
+
+        def __init__(self, master=None):
+            Frame.__init__(self, master)
+            self.pack()
+            self.createWidgets()
+
+    def on_closing():
+        if messagebox.askokcancel("Quit", "Do you want to quit?"):
+            os._exit(1)
+
+    root = Tk()
+    root.protocol("WM_DELETE_WINDOW", on_closing)
+    app = Application(master=root)
+    root.title("Nanoleaf")
+    root.geometry('+%d+%d' % (100, 100))
+    app.mainloop()
+    root.destroy()
+
 def main():
     effect_options = getEffects()
+    controlWindow(effect_options)
 
 if __name__ == "__main__":
     main()
