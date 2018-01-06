6cdc049af41fe7c9384884dfe3695bc5c6f27d2d
Robbie Lowles <robbie.windsor+git@gmail.com>
2018-01-05 23:31:00 -0500

    Addded function that returns two lists, one of normal effects and one of rhythm effects

--- Nanoleaf_Interface.py
+++ Nanoleaf_Interface.py
@@ -1,31 +1,30 @@
 from nanoleaf import setup, Aurora
+import sys
 
-ips = setup.find_auroras(5)
+ips = None
+
+while not ips:
+    ips = setup.find_auroras(1)
 token = 'z9HOW4b7ewPUvX2QKBjwVIRcOeaS1jA1'
 
 aurora = Aurora(ips[0], token)
 aurora.on = True
 #aurora.effect = "Nemo"
 
-effects = aurora.effects_list
-panels = aurora.panel_positions
-
-rhythm_effects = []
-for effect in effects:
-    try:
-        if aurora.effect_details(effect)['pluginType']:
-            rhythm_effects.append(effect)
-    except:
-        pass
-
-
-print (effects)
-print (rhythm_effects)
-
-print ("End of system")
+def getEffects():
+    all_effects = aurora.effects_list
+    rhythm_effects = []
+    effects = []
+    for effect in all_effects:
+        try:
+            if aurora.effect_details(effect)['pluginType']:
+                rhythm_effects.append(effect)
+        except:
+            effects.append(effect)
+    return [effects, rhythm_effects]
 
 def main():
-    print ("Refacotr here")
+    effect_options = getEffects()
 
-if "__name__" == "__main__":
+if __name__ == "__main__":
     main()
